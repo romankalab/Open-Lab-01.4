1e244521a22b4a131ecb927c42f7e454a533bc8b
romankalab <roan.kalab@mail.com>
2019-10-09 20:39:43 +0200

    Initial

---
+++ Open-Lab-01.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Open_Lab_01._4
+{
+    class GFG
+    {
+        public static void Main()
+        {
+            int Base;
+            Console.WriteLine("Enter the base value: ");
+            Base = Convert.ToInt32(Console.ReadLine());
+
+            int Height;
+            Console.WriteLine("Enter the height value: ");
+            Height = Convert.ToInt32(Console.ReadLine());
+
+            int TriArea = (Base * Height) / 2;
+            Console.WriteLine($"TriArea value is {TriArea}");
+        }
+    }
+}
